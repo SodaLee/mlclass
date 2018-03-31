9bb19ae6482a3feef21a9c3cac8ef70a79937f16
SodaLee <sodalee@pku.edu.cn>
2018-03-31 23:39:04 +0800

    model finished

--- week02/net.py
+++ week02/net.py
@@ -1,79 +1,92 @@
-#from __future__ import print_function, division
-import numpy as np
 import tensorflow as tf
 
-num_epochs = 10
-total_series_length = 50000
-truncated_backprop_length = 15
-state_size = 4
-num_classes = 2
-echo_step = 3
-batch_size = 5
-num_batches = total_series_length//batch_size//truncated_backprop_length
+batch_size = 10
+num_epochs = 100
+num_batches = 100
 
-def generateData():
-    x = np.array(np.random.choice(2, total_series_length, p=[0.5, 0.5]))
-    y = np.roll(x, echo_step)
-    y[0:echo_step] = 0
+def weight_variable(shape, name=None):
+	initial = tf.truncated_normal(stape, stddev=0.1)
+	return tf.get_variable(initial, name=name)
 
-    x = x.reshape((batch_size, -1))  # The first index changing slowest, subseries as rows
-    y = y.reshape((batch_size, -1))
+def conv_layer(inpt, filter_shape, stride, name=None):
+	out_channels = filter_shape[3]
+	
+	_filter = weight_variable(filter_shape)
+	conv = tf.nn.conv2d(inpt, filter=_filter, strides=[1, stride, stride, 1], padding='VALID')
+	mean, var = tf.nn.moments(conv, axes=[0,1,2])
+	beta = tf.get_variable(tf.zeros([out_channels]), name="beta"+name)
+	gamma = weight_variable([out_channels], name="gamma"+name)
+	
+	batch_norm = tf.nn.batch_norm_with_global_normalization( \
+		conv, mean, var, beta, gamma, 0.001,
+		scale_after_normalization=True)
+	
+	out = tf.nn.relu(batch_norm)
+	
+	return out
 
-    return (x, y)
+def f_net(inpt):
+	
+	conv1 = conv_layer(inpt, [8, 8, 53, 25], 1, 'conv1')
+	pool1 = tf.nn.max_pool(conv1, [1, 2, 2, 1], [1, 2, 2, 1], padding='SAME')
+	conv2 = conv_layer(inpt, [8, 8, 25, 50], 1, 'conv2')
+	
+	return conv2
 
-batchX_placeholder = tf.placeholder(tf.float32, [batch_size, truncated_backprop_length])
-batchY_placeholder = tf.placeholder(tf.int32, [batch_size, truncated_backprop_length])
+batchX_155 = tf.placeholder(tf.float32, [batch_size, 155, 155, 3])
+batchX_67 = tf.placeholder(tf.float32, [batch_size, 67, 67, 3])
+batchX_23 = tf.placeholder(tf.float32, [batch_size, 23, 23, 3])
+batchY = tf.placeholder(tf.int32, [batch_size, 80])
+#f*f*f
+empty_155 = tf.zeros([batch_size, 155, 155, 50])
+conv1_155 = f_net(tf.concat([batchX_155, empty_155], 3))
+conv2_155 = f_net(tf.concat([batchX_67, conv1_155], 3))
+conv3_155 = f_net(tf.concat([batchX_23, conv2_155], 3))
+out_155 = tf.reshape(conv3_155, [batch_size, 50])
+W_155 = weight_variable([50, 80], name='W_155')
+b_155 = tf.Variable(tf.zeros([batch_size, 80]))
+fc_155 = tf.nn.xw_plus_b(out_155, W_155, b_155)
+#f*f
+empty_67 = tf.zeros([batch_size, 67, 67, 50])
+conv1_67 = f_net(tf.concat([batchX_67, empty_67], 3))
+conv2_67 = f_net(tf.concat([batchX_23, conv1_67], 3))
+out_67 = tf.reshape(conv2_67, [batch_size, 50])
+W_67 = weight_variable([50, 80], name='W_67')
+b_67 = tf.Variable(tf.zeros([batch_size, 80]))
+fc_67 = tf.nn.xw_plus_b(out_67, W_67, b_67)
+#f
+empty_23 = tf.zeros([batch_size, 23, 23, 50])
+conv1_23 = f_net(tf.concat([batchX_23, empty_23], 3))
+out_23 = tf.reshape(conv1_23, [batch_size, 50])
+W_23 = weight_variable([50, 80], name='W_23')
+b_23 = tf.Variable(tf.zeros([batch_size, 80]))
+fc_23 = tf.nn.xw_plus_b(out_23, W_23, b_23)
 
-init_state = tf.placeholder(tf.float32, [batch_size, state_size])
+out = fc_155 + fc_67 + fc_23
 
-W = tf.Variable(np.random.rand(state_size+1, state_size), dtype=tf.float32)
-b = tf.Variable(np.zeros((1,state_size)), dtype=tf.float32)
-
-W2 = tf.Variable(np.random.rand(state_size, num_classes),dtype=tf.float32)
-b2 = tf.Variable(np.zeros((1,num_classes)), dtype=tf.float32)
-
-# Unpack columns
-inputs_series = tf.unstack(batchX_placeholder, axis=1)
-labels_series = tf.unstack(batchY_placeholder, axis=1)
-
-# Forward pass
-current_state = init_state
-states_series = []
-for current_input in inputs_series:
-    current_input = tf.reshape(current_input, [batch_size, 1])
-    input_and_state_concatenated = tf.concat([current_input, current_state], 1)  # Increasing number of columns
-
-    next_state = tf.tanh(tf.matmul(input_and_state_concatenated, W) + b)  # Broadcasted addition
-    states_series.append(next_state)
-    current_state = next_state
-
-logits_series = [tf.matmul(state, W2) + b2 for state in states_series] #Broadcasted addition
-predictions_series = [tf.nn.softmax(logits) for logits in logits_series]
-
-losses = [tf.nn.sparse_softmax_cross_entropy_with_logits(logits=log, labels=lab) for log, lab in zip(logits_series,labels_series)]
+predictions_series = [tf.nn.softmax(logits) for logits in out]
+losses = [tf.nn.sparse_softmax_cross_entropy_with_logits(logits=log, labels=lab) for log, lab in zip(out,batchY)]
 total_loss = tf.reduce_mean(losses)
 
-train_step = tf.train.AdagradOptimizer(0.3).minimize(total_loss)
-
+train_step = tf.train.AdagradOptimizer(0.02).minimize(total_loss)
 
 with tf.Session() as sess:
-    sess.run(tf.initialize_all_variables())
-    loss_list = []
+	sess.run(tf.initialize_all_variables())
 
-    for epoch_idx in range(num_epochs):
-        x,y = generateData()
-        _current_state = np.zeros((batch_size, state_size))
+	for epoch_idx in range(num_epochs):
+		x,y = generateData()
+		_current_state = np.zeros((batch_size, state_size))
 
-        print("New data, epoch", epoch_idx)
+		print("New data, epoch", epoch_idx)
 
-        for batch_idx in range(num_batches):
-            start_idx = batch_idx * truncated_backprop_length
-            end_idx = start_idx + truncated_backprop_length
+		for batch_idx in range(num_batches):
+			start_idx = batch_idx * truncated_backprop_length
+			end_idx = start_idx + truncated_backprop_length
 
-            batchX = x[:,start_idx:end_idx]
-            batchY = y[:,start_idx:end_idx]
+			batchX = x[:,start_idx:end_idx]
+			batchY = y[:,start_idx:end_idx]
 
-            _total_loss, _train_step, _current_state, _predictions_series = sess.run(
+			_total_loss, _train_step, _current_state, _predictions_series = sess.run(
                 [total_loss, train_step, current_state, predictions_series],
                 feed_dict={
                     batchX_placeholder:batchX,
@@ -81,7 +94,5 @@ with tf.Session() as sess:
                     init_state:_current_state
                 })
 
-            loss_list.append(_total_loss)
-
-            if batch_idx%100 == 0:
-                print("Step",batch_idx, "Loss", _total_loss)
+			if batch_idx%100 == 0:
+				print("Step",batch_idx, "Loss", _total_loss)
